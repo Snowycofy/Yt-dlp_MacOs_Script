f22d09f46102fed11a16bdc0327c19e378b7110f
Snowycofy <22638242+Snowycofy@users.noreply.github.com>
2025-04-01 22:54:58 +0200

    The code is a bit more clean

--- Yt-dlp_macos_script.sh
+++ Yt-dlp_macos_script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-Reslt='2'
-var='1'
+Reslt='0'
+PathToYtDlp="/Users/snowycofy/yt-dlp_macos/yt-dlp_macos"
+Output="/Users/snowycofy/Desktop/Download/%(title)s.%(ext)s"
 
-until [ $Reslt = $var ]; do
- echo -n "Youtube Link: "
+until [ $Reslt = '1' ]; do
+ echo -n "Put the youtube link or 1 to exit: "
  read Reslt
- if [ $Reslt != $var ]
+ if [ $Reslt != '1' ]
  then
-  /Users/snowycofy/yt-dlp_macos/yt-dlp_macos -x --audio-format mp3 -o "/Users/snowycofy/Desktop/Download/%(title)s.%(ext)s" $Reslt
+  $PathToYtDlp -x --audio-format mp3 -o $Output $Reslt
  fi
 done
 exit 0
